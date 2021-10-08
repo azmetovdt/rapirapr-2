37384c1c8a2474824541759e1daddede8b882db5
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 21:21:53 +0300

    Scripted auto-commit on change (2021-10-08 21:21:53) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/AirportWritable.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/AirportWritable.java
@@ -2,11 +2,15 @@ package ru.bmstu.rapirapr.azmetov.delays;
 
 import org.apache.hadoop.io.Writable;
 
+import java.io.DataInput;
 import java.io.OutputStream;
 import java.util.List;
 
 public class CSVWritable implements Writable {
     void write(OutputStream outputStream, List<String[]> datas) {
 
+    }
+    void readFields(DataInput input) {
+        conso
     }
 }

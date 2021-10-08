95fe3deb10bba8dc48f002321c29b3f438aadcaa
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 21:24:05 +0300

    Scripted auto-commit on change (2021-10-08 21:24:05) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/CSVWritable.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/CSVWritable.java
@@ -5,20 +5,11 @@ import org.apache.hadoop.io.Writable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.List;
 
 public class CSVWritable implements Writable {
-    void write(OutputStream outputStream) {
-
-    }
-    void readFields(String s) {
-        conso
-    }
-
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-
+        
     }
 
     @Override

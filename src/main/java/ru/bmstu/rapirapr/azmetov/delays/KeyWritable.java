850edfc64c8461c86b67af97e5657f1cc5f3bfef
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 22:10:06 +0300

    Scripted auto-commit on change (2021-10-08 22:10:06) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/KeyWritable.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/KeyWritable.java
@@ -8,6 +8,9 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class KeyWritable implements WritableComparable {
+    private static final int isAirport = 0;
+
+    
     @Override
     public int compareTo(Object o) {
         return 0;

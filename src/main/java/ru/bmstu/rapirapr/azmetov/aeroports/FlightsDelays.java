210e7da6f3e2e9b31177f58b7c55e3f6440a4a79
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 19:57:03 +0300

    Scripted auto-commit on change (2021-10-08 19:57:03) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/aeroports/FlightsDelays.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/aeroports/FlightsDelays.java
@@ -1,12 +1,18 @@
 package ru.bmstu.rapirapr.azmetov.aeroports;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCountApp {
-    public static final String jobName = "Word count";
+    public static final String jobName = "DelaysStatisticts";
 
     public static void main(String[] args) throws Exception {
         if (args.length != 2) {
-            System.err.println("Usage: ru.bmstu.rapirapr.azmetov.warandpeace.WordCountApp <input path> <output path>");
+            System.err.println("Usage: ru.bmstu.rapirapr.azmetov.warandpeace.DelaysStatisticts <input path> <output path>");
             System.exit(-1);
         }
         Job job = Job.getInstance();

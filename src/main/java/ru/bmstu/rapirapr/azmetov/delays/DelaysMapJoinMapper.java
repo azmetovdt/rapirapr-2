b351b0f247b0ea9b07efc8f662ccaba0c333c8af
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 22:45:56 +0300

    Scripted auto-commit on change (2021-10-08 22:45:56) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysMapJoinMapper.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysMapJoinMapper.java
@@ -6,15 +6,14 @@ import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.join.TupleWritable;
 
 import java.io.IOException;
 
-public class DelaysMapJoinMapper extends MapReduceBase implements Mapper<Text, TupleWritable, Text, Text> {
+public class DelaysMapJoinMapper extends MapReduceBase implements Mapper<Text, Text, Text, Text> {
     @Override
-    public void map(Text key, TupleWritable value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-        Text a = (Text) value.get(0);
-        Text b = (Text) value.get(1);
+    public void map(Text key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+        Text a = (Text) value;
+        Text b = (Text) value;
         output.collect(a, b);
     }
 

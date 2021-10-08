86ae0b5534b570105197a2708fc3895696035bc2
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 22:05:51 +0300

    Scripted auto-commit on change (2021-10-08 22:05:51) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/JoinReducer.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/JoinReducer.java
@@ -1,4 +1,15 @@
 package ru.bmstu.rapirapr.azmetov.delays;
 
-public class JoinReducer {
+public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+            IOException, InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
 }

ebffa8f433c8aeaebbb75b8cdfc0649a57ece5f7
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 20:46:59 +0300

    Scripted auto-commit on change (2021-10-08 20:46:59) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -20,7 +20,7 @@ public class DelaysStatisticsApp {
         Job job = Job.getInstance(conf);
 
         conf.setInputFormat(CompositeInputFormat.class);
-        FileOutputFormat.setOutputPath(Job.getInstance(conf), new Path(args[2]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
         conf.set("mapred.join.expr", CompositeInputFormat.compose("inner",
                 KeyValueTextInputFormat.class,
                 args[0],

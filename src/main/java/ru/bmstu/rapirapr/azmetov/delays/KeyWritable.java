9e1800aed12d15f94ceb14b211b4ca738538497c
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 22:09:29 +0300

    Scripted auto-commit on change (2021-10-08 22:09:29) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/KeyWritable.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/KeyWritable.java
@@ -3,5 +3,23 @@ package ru.bmstu.rapirapr.azmetov.delays;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class KeyWritable implements WritableComparable {
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }

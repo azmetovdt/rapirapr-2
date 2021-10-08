2b63aaa4d42b36a63d19be9426d370b5b33a2efe
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 22:38:34 +0300

    Scripted auto-commit on change (2021-10-08 22:38:34) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -3,7 +3,6 @@ package ru.bmstu.rapirapr.azmetov.delays;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.KeyValueTextInputFormat;
 import org.apache.hadoop.mapred.join.CompositeInputFormat;
@@ -26,11 +25,12 @@ public class DelaysStatisticsApp {
                 args[1]
         ));
         conf.setMapperClass(DelaysMapJoinMapper.class);
-        Job job = Job.getInstance(conf);
-        job.setReducerClass(JoinReducer.class);
         conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(Text.class);
-        job.
-        JobClient.runJob(conf);
+        Job job = Job.getInstance(conf);
+        job.setReducerClass(JoinReducer.class);
+
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }

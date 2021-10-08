69829f4737efbb4eb2ff63a6e3f9dd3e69ba8650
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 21:37:41 +0300

    Scripted auto-commit on change (2021-10-08 21:37:41) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightReducer.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightReducer.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 import java.io.IOException;
 import java.util.Iterator;
 
-public class FlightReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+public class FlightReducer extends Reducer<Text,  IntWritable, Text, LongWritable> {
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws
     IOException, InterruptedException {

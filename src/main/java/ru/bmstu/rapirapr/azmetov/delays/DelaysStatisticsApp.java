378a67993e72171ce0027967fb62dbfe15f232f6
Azmetov Danila <ohstapit@gmail.com>
2021-10-08 20:10:37 +0300

    Scripted auto-commit on change (2021-10-08 20:10:37) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -2,7 +2,10 @@ package ru.bmstu.rapirapr.azmetov.delays;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.KeyValueTextInputFormat;
+import org.apache.hadoop.mapred.join.CompositeInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class DelaysStatisticsApp {
@@ -13,7 +16,7 @@ public class DelaysStatisticsApp {
             System.err.println("Usage: ru.bmstu.rapirapr.azmetov.warandpeace.DelaysStatistics <input path> <output path>");
             System.exit(-1);
         }
-        JobConf conf = new JobConf(JoinJob.class);
+        JobConf conf = new JobConf(DelaysStatisticsApp.class);
 
         conf.setInputFormat(CompositeInputFormat.class);
         FileOutputFormat.setOutputPath(conf, new Path(args[2]));

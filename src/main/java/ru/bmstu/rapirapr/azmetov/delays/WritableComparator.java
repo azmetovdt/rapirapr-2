4efa233825e3fb97a72089c07dc438f38a18377d
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 18:36:47 +0300

    Scripted auto-commit on change (2021-10-14 18:36:47) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/WritableComparator.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/WritableComparator.java
@@ -8,7 +8,7 @@ public class WritableComparator implements RawComparator<WritableComparable> {
 
     @Override
     public int compare(WritableComparable o1, WritableComparable o2) {
-        return 0;
+        return o1.compareTo(o2);
     }
 
     @Override

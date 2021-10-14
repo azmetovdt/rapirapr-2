0d1522b0b29dc164e27a55c65c9097dde81760b3
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 18:38:22 +0300

    Scripted auto-commit on change (2021-10-14 18:38:22) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightsJoinMapper.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightsJoinMapper.java
@@ -1,6 +1,31 @@
 package ru.bmstu.rapirapr.azmetov.delays;
 
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class SystemsJoinMapper extends Mapper {
+import java.io.IOException;
+
+public class FlightsJoinMapper extends Mapper<LongWritable, Text, KeyWritable, FlightWritable> {
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] csvColumns = value.toString().split(",");
+        context.write(new KeyWritable(csvColumns[0],true), new FlightWritable(csvColumns[18]));
+    }
+       /* String value = "";
+        String airportCode = "";
+        if (key.toString().charAt(0) == '"') {
+            value = key.toString().split(",")[1];
+        } else if (key.toString().split(",").length > 18) {
+            airportCode = key.toString().split(",")[13];
+            value =  key.toString().split(",")[18];
+
+        } else {
+            value =  key.toString();
+        }
+
+        output.collect(new Text(airportCode), new Text(value));
+    }
+ */
 }

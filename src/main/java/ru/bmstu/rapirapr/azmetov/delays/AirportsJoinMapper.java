a6ed92d3a5516f71fe2418f2851a6cf964773b07
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 19:01:27 +0300

    Scripted auto-commit on change (2021-10-14 19:01:27) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/AirportsJoinMapper.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/AirportsJoinMapper.java
@@ -12,7 +12,7 @@ public class AirportsJoinMapper extends Mapper<LongWritable, Text, KeyWritable, 
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] csvColumns = value.toString().split(",", 2);
         context.write(new KeyWritable(csvColumns[0],true),
-               csvColumns.length > 1 ?  new AirportWritable(csvColumns[1]).airportName : new AirportWritable("EXCEPTION").airportName);
+                new AirportWritable(csvColumns[1]).airportName;
     }
        /* String value = "";
         String airportCode = "";

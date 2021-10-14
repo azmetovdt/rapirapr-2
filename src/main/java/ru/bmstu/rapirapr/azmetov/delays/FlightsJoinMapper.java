6ab16f54297981577f7cad005995ba71fff44cb0
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 22:49:43 +0300

    Scripted auto-commit on change (2021-10-14 22:49:43) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightsJoinMapper.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightsJoinMapper.java
@@ -1,6 +1,7 @@
 package ru.bmstu.rapirapr.azmetov.delays;
 
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,7 +13,7 @@ public class FlightsJoinMapper extends Mapper<LongWritable, Text, KeyWritable, T
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] csvColumns = value.toString().split(",");
         String delay = csvColumns[14];
-        if (delay != "0.00") {
+        if (delay != "0.00" && !StringUtils.isBlank(delay)) {
             context.write(new KeyWritable(delay, true), new FlightWritable(csvColumns[18]).delay);
         }
     }

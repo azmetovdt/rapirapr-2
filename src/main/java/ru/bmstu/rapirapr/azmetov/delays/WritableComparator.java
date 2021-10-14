2f37c7b9982e702adce168bd45035b72dc6559ef
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 20:18:01 +0300

    Scripted auto-commit on change (2021-10-14 20:18:01) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/WritableComparator.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/WritableComparator.java
@@ -3,7 +3,7 @@ package ru.bmstu.rapirapr.azmetov.delays;
 import org.apache.hadoop.io.RawComparator;
 import org.apache.hadoop.io.WritableComparable;
 
-public class WritableComparator implements RawComparator {
+public class WritableComparator implements RawComparator<WritableComparable> {
 
 
     @Override
@@ -12,11 +12,6 @@ public class WritableComparator implements RawComparator {
     }
 
     public int compare(byte[] bytes, int i, int i1, byte[] bytes1, int i2, int i3) {
-        return 0;
-    }
-
-    @Override
-    public int compare(Object o1, Object o2) {
         return 0;
     }
 }

72ccb6d1a496014d18cbad045f2fc009116a3e8c
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 19:23:21 +0300

    Scripted auto-commit on change (2021-10-14 19:23:21) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -3,8 +3,8 @@ package ru.bmstu.rapirapr.azmetov.delays;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class DelaysStatisticsApp {
@@ -17,8 +17,8 @@ public class DelaysStatisticsApp {
         Job job = Job.getInstance();
         job.setJarByClass(DelaysStatisticsApp.class);
         job.setJobName("DelaysStatisticsApp");
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsJoinMapper.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]), FileInputFormat.class, AirportsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), FileInputFormat.class, FlightsJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(HashPartitioner.class);
         job.setGroupingComparatorClass(WritableComparator.class);

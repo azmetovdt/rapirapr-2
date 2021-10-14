e758cb96fd730e34aeddfbe94a7a1513a40ea2a2
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 21:01:47 +0300

    Scripted auto-commit on change (2021-10-14 21:01:47) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/Comparator.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/Comparator.java
@@ -9,7 +9,8 @@ public class Comparator extends WritableComparator {
     }
     @Override
     public int compare(WritableComparable o1, WritableComparable o2)  {
-        k1 
-        return o1.airportId.compareTo(o2.airportId);
+        KeyWritable k1 = (KeyWritable) o1;
+        KeyWritable k2 = (KeyWritable) o2;
+        return k2.airportId.compareTo(k2.airportId);
     }
 }

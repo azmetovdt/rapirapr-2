ddd5d120b6569aea488240c1ed5f2ad30f351a8a
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 20:55:31 +0300

    Scripted auto-commit on change (2021-10-14 20:55:31) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/Comparator.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/Comparator.java
@@ -3,9 +3,11 @@ package ru.bmstu.rapirapr.azmetov.delays;
 import org.apache.hadoop.io.WritableComparator;
 
 public class Comparator extends WritableComparator {
+    protected Comparator() {
 
+    }
     public int compare(KeyWritable o1, KeyWritable o2)  {
-        protected
+
 
         return o1.airportId.compareTo(o2.airportId);
     }

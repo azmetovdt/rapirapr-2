ae3b571885172550368ff6472eef3ab8cac14886
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 17:09:22 +0300

    Scripted auto-commit on change (2021-10-14 17:09:22) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightsJoinMapper.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/FlightsJoinMapper.java
@@ -1,19 +1,17 @@
 package ru.bmstu.rapirapr.azmetov.delays;
 
 
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.join.TupleWritable;
+import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
-public class FlightsJoinMapper extends Mapper implements Mapper<Text, TupleWritable, Text, Text> {
+public class FlightsJoinMapper extends Mapper<LongWritable, TupleWritable, Text, Text> {
     @Override
-    public void map(Text key, TupleWritable v, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
-        String value = "";
+    public void map(LongWritable key, Text value, Context context) throws IOException {
+       /* String value = "";
         String airportCode = "";
         if (key.toString().charAt(0) == '"') {
             value = key.toString().split(",")[1];
@@ -27,5 +25,5 @@ public class FlightsJoinMapper extends Mapper implements Mapper<Text, TupleWrita
 
         output.collect(new Text(airportCode), new Text(value));
     }
-
+ */
 }

f3445ab8f79bc5fe18fe4598eace5d1052131025
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 17:37:46 +0300

    Scripted auto-commit on change (2021-10-14 17:37:46) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/AirportWritable.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/AirportWritable.java
@@ -1,6 +1,22 @@
 package ru.bmstu.rapirapr.azmetov.delays;
 
-public class AirportWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportWritable extends Writable {
     public AirportWritable(String csvColumn) {
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
     }
 }

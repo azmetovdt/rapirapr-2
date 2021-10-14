7697a829a56ab92df97fa62bbe2f23a149aa6e55
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 21:37:39 +0300

    Scripted auto-commit on change (2021-10-14 21:37:39) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -21,14 +21,14 @@ public class DelaysStatisticsApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        //job.setPartitionerClass(MyPartitioner.class);
+        job.setPartitionerClass(MyPartitioner.class);
         job.setGroupingComparatorClass(Comparator.class);
         job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(KeyWritable.class);
         job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        //job.setNumReduceTasks(2);
+        job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }

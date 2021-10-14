65f4954c992c43cd237d63ab04cd3ce507d73dfc
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 16:53:49 +0300

    Scripted auto-commit on change (2021-10-14 16:53:49) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -15,21 +15,18 @@ public class DelaysStatisticsApp {
             System.exit(-1);
         }
 
-        JobConf conf = new JobConf(DelaysStatisticsApp.class);
-
-        conf.setInputFormat(CompositeInputFormat.class);
-        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
-        conf.set("mapred.join.expr", CompositeInputFormat.compose("outer",
-                KeyValueTextInputFormat.class,
-                args[0],
-                args[1]
-        ));
-        conf.setMapperClass(DelaysMapJoinMapper.class);
-        conf.setOutputKeyClass(Text.class);
-        conf.setOutputValueClass(Text.class);
-        Job job = Job.getInstance(conf);
+        Job job = Job.getInstance();
+        job.setJarByClass(JoinJob.class);
+        job.setJobName("JoinJob sort");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
-
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }

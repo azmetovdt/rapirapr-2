9cc5a0d47f9d9888965535a486988f08604c889e
Azmetov Danila <ohstapit@gmail.com>
2021-10-14 21:19:02 +0300

    Scripted auto-commit on change (2021-10-14 21:19:02) by gitwatch.sh

---
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/MyPartitioner.java
@@ -0,0 +1,12 @@
+package ru.bmstu.rapirapr.azmetov.delays;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+
+public class MyPartitioner extends HashPartitioner<KeyWritable, Text> {
+
+    public int getPartition(KeyWritable key, Text value, int numReduceTasks) {
+        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
+}

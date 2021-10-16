746f30011a466e7a788d4b99090ba80b6f3a68de
Azmetov Danila <ohstapit@gmail.com>
2021-10-16 12:53:05 +0300

    Scripted auto-commit on change (2021-10-16 12:53:05) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/DelaysStatisticsApp.java
@@ -8,15 +8,18 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class DelaysStatisticsApp {
+    public static final String JOB_NAME = "DelaysStatisticsApp";
+    public static final String USAGE_ERROR_TEXT = "Usage: ru.bmstu.rapirapr.azmetov.delays.DelaysStatistics <input1 path> <input2 path> <output path>";
+
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.err.println("Usage: ru.bmstu.rapirapr.azmetov.warandpeace.DelaysStatistics <input1 path> <input2 path> <output path>");
+            System.err.println(USAGE_ERROR_TEXT);
             System.exit(-1);
         }
 
         Job job = Job.getInstance();
         job.setJarByClass(DelaysStatisticsApp.class);
-        job.setJobName("DelaysStatisticsApp");
+        job.setJobName(JOB_NAME);
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirportsJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FlightsJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));

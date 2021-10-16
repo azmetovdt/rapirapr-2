6933402ca1ab917419c98a1afa20c964222e93b9
Azmetov Danila <ohstapit@gmail.com>
2021-10-16 13:01:06 +0300

    Scripted auto-commit on change (2021-10-16 13:01:06) by gitwatch.sh

--- src/main/java/ru/bmstu/rapirapr/azmetov/delays/JoinReducer.java
+++ src/main/java/ru/bmstu/rapirapr/azmetov/delays/JoinReducer.java
@@ -18,7 +18,7 @@ public class JoinReducer extends Reducer<KeyWritable, Text, Text, Text> {
         while (valuesIterator.hasNext()) {
             String value = valuesIterator.next().toString();
             if (!NumberUtils.isParsable(value)) {
-                airportName = value;
+                airportName = value; 
             } else {
                 float delay = Float.parseFloat(value);
                 minDelay = Math.min(minDelay, delay);
